2bf8119ebd9ed00bced3e4dbf6b84e8ac157115e
Joy Das <joydascsepuc@gmail.com>
2022-11-10 22:37:22 +0600

    added routes for fb login

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SocialLoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('auth/facebook', [SocialLoginController::class, 'facebookRedirect']);
+Route::get('auth/facebook/callback', [SocialLoginController::class, 'loginWithFacebook']);
