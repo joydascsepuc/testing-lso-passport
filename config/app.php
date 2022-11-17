560b0d35c46a1c6589fa70e9b0e030f513fabd5a
Joy Das <joydascsepuc@gmail.com>
2022-11-17 21:18:03 +0600

    configured the passport in the application

--- config/app.php
+++ config/app.php
@@ -187,6 +187,7 @@ return [
          */
 
         Laravel\Socialite\SocialiteServiceProvider::class,
+        Laravel\Passport\PassportServiceProvider::class,
 
         /*
          * Application Service Providers...
